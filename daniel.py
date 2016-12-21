442c02c7337a69467082333de9c2ff9b8776f482
bolandka <bolandka>
2016-12-21 12:00:43 +0000

    daniel can now use mp3s

--- daniel.py
+++ daniel.py
@@ -3,12 +3,13 @@ from client import jasperpath
 import os
 import random
 
-def getRandomSong():
-    dir = jasperpath.data('daniel')
+def getRandomSong(format):
+    dir = jasperpath.data('daniel', format)
     return os.path.join(dir, random.choice(os.listdir(dir)))
 
 def run(mic):
-    mic.speaker.play(getRandomSong())
+    mic.speaker.play(getRandomSong("wav"))
 
-    
-	
+
+if __name__=="__main__":
+    os.system("mpg321 " + getRandomSong("mp3").replace(" ", "\ "))
